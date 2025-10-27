5c50752b23ef05cb4f6256379ff249aaa02f6805
tamsynC <tamsyn.m.crangle@student.uts.edu.au>
2025-10-27 15:42:57 +1100

    grid

--- FinalInterface/finalInterface.py
+++ FinalInterface/finalInterface.py
@@ -15,29 +15,36 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle(string_main_title)
         self.setWindowIcon(QIcon("Window_Icon.png"))
-
+        self.tool_bar()
         self.main_menu()
 
-
-
-    def main_menu(self):
+    def tool_bar(self):
 
         # Create Toolbar
         toolbar = QToolBar("Main Toolbar")
         toolbar.setIconSize(QSize(50, 50))
         self.addToolBar(toolbar)
 
+        spacer1 = QWidget()
+        spacer1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        toolbar.addWidget(spacer1)
+
         title = QLabel("Project Title")
 
-        title.setStyleSheet()
+        title.setStyleSheet("font-size: 30px;"
+                            "font-weight: bold")
 
         toolbar.addWidget(title)
 
+        spacer2 = QWidget()
+        spacer2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        toolbar.addWidget(spacer2)
 
         # Home Buton - Toolbar
         action_home = QAction(QIcon("home.png"), "Home", self)
         action_home.triggered.connect(self.on_home)
         toolbar.addAction(action_home)
+        
 
         # Map Button - Toolbar
         action_map = QAction(QIcon("map.png"), "Map", self)
@@ -49,13 +56,28 @@ class MainWindow(QMainWindow):
         action_camera.triggered.connect(self.on_camera)
         toolbar.addAction(action_camera)
 
-        # --- Add a central widget ---
+    def main_menu(self):
+
         central_widget = QWidget()
-        layout = QVBoxLayout()
-        button = QPushButton("Click Me!")
-        layout.addWidget(button)
-        central_widget.setLayout(layout)
+        grid = QGridLayout()
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(QPushButton("A"))
+        hbox.addWidget(QPushButton("B"))
+        hbox.addWidget(QPushButton("C"))
+
+        grid.addLayout(hbox, 0,0)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(QPushButton("Top"))
+        vbox.addWidget(QPushButton("Bottom"))
+
+        grid.addLayout(vbox, 0, 1)
+
+        central_widget.setLayout(grid)
+
         self.setCentralWidget(central_widget)
+
 
     def on_home(self):
         print("Home clicked")
